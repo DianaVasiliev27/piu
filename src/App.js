73f6d281bb785654e1d6592a460e51d8c8767344
DianaVasiliev27 <57351800+DianaVasiliev27@users.noreply.github.com>
2021-05-31 13:10:09 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Nav from "./nav";
+import Container from "react-bootstrap/Container";
+import Card from "./card.js";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Container style={{ marginTop: "30px" }}>
+        <Card />
+      </Container>
+    </div>
+  );
+}
